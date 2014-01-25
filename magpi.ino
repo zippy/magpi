ae995f31bab10496ccfb4a811ea04dfd24c052d2
Will Harris-Braun <githubwj@harris-braun.com>
2014-01-25 17:41:02 -0500

    Created Branch and started on a new game (rotate-y box game)

--- magpi.ino
+++ magpi.ino
@@ -34,20 +34,17 @@ typedef struct {
 Game;
 
 #define MENU_GAME    0
-#define CATCHER_GAME 1
-#define DRAW_GAME    2
-#define SNAKE_GAME   3
-#define OPTIONS_GAME 4
+#define DRAW_GAME    1
+#define TEST_GAME   2
+#define OPTIONS_GAME 3
 
 const int game_count = OPTIONS_GAME+1;
 Game games[game_count] = {
   {menu,menu_init,0}
   ,
-  {catcher,catcher_init,catcher_menu}
-  ,
   {drawer,drawer_init,drawer_menu}
   ,
-  {snake,snake_init,snake_menu}
+  {test,test_init,test_menu}
   ,
   {options,options_init,options_menu}
 };
@@ -94,6 +91,21 @@ const unsigned char PROGMEM  sys_splash[] = {
 
 //---------------------------------------------------------------------
 // UTILS
+
+long dt;
+
+uint8_t level;
+
+void do_splash(const unsigned char *s) {
+  if (splash) {
+    splash = false;
+    display.clearDisplay();
+    display.drawBitmap(0,0,s,W,H,1);
+    display.display();
+    while(!pad_check()) {menu_check();};
+  }
+}
+
 #define PAD_U_PIN 7
 #define PAD_U 0x01
 #define PAD_D_PIN 8
@@ -323,423 +335,6 @@ void options_init() {
   current_option=0;
 }
 
-//---------------------------------------------------------------------
-// CATCHER
-/*
-0xE1,0xB0,0x98,0x8C,0x86,0xC3,0x0,0x0,0x0,0x0,
- 0x9F,0xCF,0xE7,0xF3,0xF9,0xBD,0x0,0x0,0x0,0x0,
- 0x81,0x81,0x81,0x81,0x81,0x81,0x0,0x0,0x0,0x0,
- 0xE1,0xB1,0x99,0x8D,0x87,0xC3,0x0,0x0,0x0,0x0,
- 0x9E,0xCF,0xE7,0xF3,0xF9,0xBC,0x0,0x0,0x0,0x0,
- 0x80,0x80,0x80,0x80,0x80,0x80,0x0,0x0,0x0,0x0,
- 0x80,0x80,0x80,0x80,0x80,0x80,0x0,0x0,0x0,0x0,
- 0x80,0x80,0x80,0x80,0x80,0x80,0x0,0x0,0x0,0x0,
- 
- 0x80,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
- 0xC0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
- 0xA0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
- 0xA0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
- 0x90,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
- 0xF0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
- 0x80,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
- 0x80,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
- */
-
-const unsigned char PROGMEM  catcher_splash[] = {
-
-  0x73,0x11,0x1C,0x19,0xC4,0x68,0x0,0x0,0x0,0x0,0x0,
-  0x94,0xBB,0xA4,0x22,0x4E,0x88,0x0,0x0,0x0,0x0,0x0,
-  0x94,0x91,0x24,0x22,0x44,0x8E,0x0,0x0,0x7,0x80,0x0,
-  0x73,0x11,0x1E,0x19,0xE4,0x6A,0x0,0x0,0xF,0xC0,0x0,
-  0x10,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x78,0x78,0x0,
-  0x72,0x60,0x0,0x0,0x0,0x0,0x0,0x0,0x78,0x78,0x0,
-  0x2,0xAF,0x7,0x24,0x80,0x0,0x0,0x0,0xF,0xC0,0x0,
-  0x0,0xCA,0x89,0x24,0x80,0x0,0x0,0x0,0x7,0x80,0x0,
-  0x0,0x8A,0x89,0x24,0x3,0xC0,0x0,0x0,0x0,0x0,0x0,
-  0x0,0x60,0x7,0xB6,0x87,0xE0,0x0,0x0,0x0,0x0,0x0,
-  0x0,0x0,0x0,0x0,0x3C,0x3C,0x0,0x0,0x0,0x0,0x0,
-  0x0,0x0,0x0,0x0,0x3C,0x3C,0x0,0x0,0x0,0x0,0x0,
-  0x0,0x0,0x0,0x0,0x7,0xE0,0x0,0x0,0x0,0x0,0x0,
-  0x0,0x0,0x0,0x0,0x3,0xC0,0x0,0x0,0x0,0x0,0x0,
-  0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-  0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-  0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-  0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-  0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-  0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-  0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-  0x0,0x0,0x0,0x1E,0x0,0x78,0x0,0x0,0x0,0x0,0x0,
-  0x0,0x0,0x0,0x1E,0x0,0x78,0x0,0x0,0x0,0x0,0x0,
-  0x0,0x0,0x0,0x1E,0x0,0x78,0x0,0x0,0x0,0x0,0x0,
-  0x0,0x0,0x0,0x1E,0x0,0x78,0x0,0x0,0x0,0x0,0x0,
-  0x0,0x0,0x0,0xF,0xFF,0xF0,0x0,0x0,0x0,0x0,0x0,
-  0x0,0x0,0x0,0x7,0xFF,0xE0,0x0,0x0,0x0,0x0,0x0,
-  0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-  0x0,0x0,0x0,0x0,0x55,0x0,0x0,0x0,0x13,0x0,0x0,
-  0x0,0x0,0x0,0x0,0x2B,0x0,0x0,0x0,0x1C,0xE0,0x0,
-  0x0,0x0,0x0,0x0,0x1A,0x0,0x0,0x0,0x10,0x20,0x0,
-  0x0,0x0,0x0,0x0,0x14,0x0,0x0,0x0,0x13,0x20,0x0,
-  0x0,0x0,0x0,0x0,0x8,0x0,0x0,0x0,0x1C,0xE0,0x0,
-  0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x10,0x0,0x0,
-  0x0,0x7F,0x0,0x0,0x8,0x0,0x0,0x0,0x10,0x0,0x0,
-  0x0,0xFF,0x0,0x0,0x0,0x0,0x0,0x0,0x10,0x0,0x0,
-  0x1,0xFE,0x7,0xC7,0xF8,0xFC,0xC6,0x7E,0x7E,0x18,0x0,
-  0x1,0xE0,0xF,0xE3,0xF1,0xF8,0xC6,0x7C,0x7F,0x18,0x0,
-  0x1,0xC0,0xC,0x60,0xC1,0xC0,0xC6,0x60,0x63,0x18,0x0,
-  0x1,0xC0,0xC,0x60,0xC1,0x80,0xFE,0x78,0x63,0x18,0x0,
-  0x1,0xC0,0xF,0xE0,0xC1,0x80,0xEE,0x78,0x7E,0x18,0x0,
-  0x1,0xC0,0xE,0xE0,0xC1,0x80,0xC6,0x60,0x7E,0x18,0x0,
-  0x1,0xC0,0xC,0x60,0xC1,0xC0,0xC6,0x60,0x67,0x18,0x0,
-  0x1,0xE0,0xC,0x60,0xC1,0xF8,0xC6,0x7C,0x63,0x80,0x0,
-  0x1,0xFE,0x8,0x20,0xC0,0xFC,0x82,0x7E,0x61,0xD8,0x0,
-  0x0,0xFF,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-  0x0,0x7F,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xF8,0x0,
-  0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-
-};
-
-
-const unsigned char PROGMEM catcher_bm[] = {
-  B11000011,
-  B11000011,
-  B01111110,
-};
-
-const unsigned char PROGMEM ball_bm[] =
-{
-  B00011000,
-  B01100110,
-  B00011000,
-};
-
-const unsigned char PROGMEM flag0_bm[] = {
-  0x80,0xC0,0xa0,0xA0,0x90,0xF0,0x80,0x80};
-const unsigned char PROGMEM flag1_bm[] = {
-  0xE1,0x9F,0x81,0xE1,0x9E,0x80,0x80,0x80};
-const unsigned char PROGMEM flag2_bm[] = {
-  0xB0,0xCF,0x81,0xB1,0xCF,0x80,0x80,0x80};
-const unsigned char PROGMEM flag3_bm[] = {
-  0x98,0xE7,0x81,0x99,0xE7,0x80,0x80,0x80};
-const unsigned char PROGMEM flag4_bm[] = {
-  0x8C,0xF3,0x81,0x8D,0xF3,0x80,0x80,0x80};
-const unsigned char PROGMEM flag5_bm[] = {
-  0x86,0xF9,0x81,0x87,0xF9,0x80,0x80,0x80};
-const unsigned char PROGMEM flag6_bm[] = {
-  0xC3,0xBD,0x81,0xC3,0xBC,0x80,0x80,0x80};
-
-#define FLAG_COUNT 5
-const unsigned char *flags_bm[] = {
-  flag1_bm,flag2_bm,flag3_bm,flag4_bm,flag5_bm,flag6_bm};
-
-
-
-#define FRAMES_PER_SECOND 10
-
-#define cW 8
-#define cH 3
-
-#define R .2  // rocket impulse
-
-#define MAX_AX 2  // max accellerations
-#define MAX_AY 1.5
-
-typedef struct sprite sprite;
-
-struct sprite {
-  float x;
-  float y;
-  float ax;
-  float ay;
-  int h;
-  int w;
-  const unsigned char  *bitmap;
-  void (*draw_fun)(void *);
-};
-
-const int ms_per_frame = 1000/FRAMES_PER_SECOND;
-
-#define MAX_SPRITES 10
-#define CATCHER 0
-sprite sprites[MAX_SPRITES];
-int sprite_count = 1;
-float gravity;
-float air_resistance;
-float wind;
-
-int score;
-
-uint8_t level,level_balls,balls_missed,frame;
-boolean level_up;
-int next_ball_max;
-
-void reset_ball(int i) {
-  sprites[i].x = random(0,W-cW-1);
-  sprites[i].y = 0;
-  sprites[i].ax = 0;
-  sprites[i].ay = 0;
-  sprites[i].w = cW;
-  sprites[i].h = cH;
-  sprites[i].bitmap = ball_bm;
-  sprites[i].draw_fun = 0;
-}
-
-void add_ball() {
-  reset_ball(sprite_count);
-  sprite_count++;
-}
-
-void kill_ball(int b) {
-  sprite_count--;
-  for(int i=b;i<sprite_count;i++) {
-    sprites[i] = sprites[i+1];
-  }
-  if (level_balls == 0 && sprite_count == 1) {
-    level_up = true;
-  }
-}
-
-void reset_catcher() {
-  sprites[CATCHER].x = W/2;
-  sprites[CATCHER].y = H/2;
-  sprites[CATCHER].ax = 0;
-  sprites[CATCHER].ay = 0;
-}
-
-/* pants */void draw_catcher(void *s) {
-  int i = frame &1;
-  switch (pad_hit) {
-  case PAD_D:
-    switch(i) {
-    case 0:
-      display.drawPixel(((sprite *)s)->x+2,((sprite *)s)->y+3,1);
-      display.drawPixel(((sprite *)s)->x+5,((sprite *)s)->y+3,1);
-      display.drawPixel(((sprite *)s)->x+3,((sprite *)s)->y+4,1);
-      display.drawPixel(((sprite *)s)->x+4,((sprite *)s)->y+3,1);
-      display.drawPixel(((sprite *)s)->x+3,((sprite *)s)->y+5,1);
-      display.drawPixel(((sprite *)s)->x+4,((sprite *)s)->y+4,1);
-      break;
-    case 1:
-      display.drawPixel(((sprite *)s)->x+2,((sprite *)s)->y+4,1);
-      display.drawPixel(((sprite *)s)->x+5,((sprite *)s)->y+4,1);
-      display.drawPixel(((sprite *)s)->x+3,((sprite *)s)->y+3,1);
-      display.drawPixel(((sprite *)s)->x+4,((sprite *)s)->y+4,1);
-      display.drawPixel(((sprite *)s)->x+3,((sprite *)s)->y+4,1);
-      display.drawPixel(((sprite *)s)->x+4,((sprite *)s)->y+5,1);
-      break;
-    }
-    break;
-  case PAD_L:
-    display.drawPixel(((sprite *)s)->x-1-i,((sprite *)s)->y+0,1);
-    display.drawPixel(((sprite *)s)->x-2-i,((sprite *)s)->y+1,1);
-    display.drawPixel(((sprite *)s)->x-1-i,((sprite *)s)->y+2,1);
-    break;
-  case PAD_R:
-    display.drawPixel(((sprite *)s)->x+8+i,((sprite *)s)->y+0,1);
-    display.drawPixel(((sprite *)s)->x+9+i,((sprite *)s)->y+1,1);
-    display.drawPixel(((sprite *)s)->x+8+i,((sprite *)s)->y+2,1);
-    break;
-  }
-}
-
-void do_splash(const unsigned char *s) {
-  if (splash) {
-    splash = false;
-    display.clearDisplay();
-    display.drawBitmap(0,0,s,W,H,1);
-    display.display();
-    while(!pad_check()) {menu_check();};
-  }
-}
-
-void catcher_init() {
-  do_splash(catcher_splash);
-  score = 0;
-  sprite_count = 1;
-  reset_catcher();
-  sprites[CATCHER].w = cW;
-  sprites[CATCHER].h = cH;
-  sprites[CATCHER].bitmap = catcher_bm;
-  sprites[CATCHER].draw_fun = &draw_catcher;
-
-  add_ball();
-
-  gravity = .005;
-  air_resistance = .005;
-  wind = 0;
-  ft = millis()+ ms_per_frame;
-  level = 0;
-  balls_missed = 0;
-  level_up = true;
-  frame = level_balls = 0;
-  next_ball_max = 10000;
-  randomSeed(ft);
-}
-
-void move() {
-  for(int i=0;i< sprite_count;i++) {
-
-    // environmental factors: gravity wind and air resistance
-    sprites[i].ay += gravity;
-    if (wind > 0) {
-      if ((wind - sprites[i].ax) > 0) sprites[i].ax += wind/50;
-    }
-    else if (wind < 0) {
-      if ((wind - sprites[i].ax) < 0) sprites[i].ax += wind/50;
-    }
-    if (sprites[i].ax > 0) sprites[i].ax -= air_resistance;
-    if (sprites[i].ax < 0) sprites[i].ax += air_resistance;
-
-    // sprite inertia wrapping and bouncing
-    sprites[i].x += sprites[i].ax;
-    sprites[i].y += sprites[i].ay;
-    int w = sprites[i].w;
-    int h = sprites[i].h;
-    float x = sprites[i].x;
-    float y = sprites[i].y;
-    if (x < 0) sprites[i].x = (W-w)-.1;
-    if (x >= W-w) sprites[i].x = 0;
-    if (y < 0) sprites[i].y = (H-h)-.1;
-    if (y >= H-h) {
-      sprites[i].ay /= 2.5; 
-      sprites[i].ay *= -1;
-      sprites[i].y += sprites[i].ay;
-      if (y>= H-h) {
-        sprites[i].y = H-h-1;
-      }
-    }
-
-    // ball collisions
-    if (i > 0) {
-      float x0 = sprites[0].x;
-      float y0 = sprites[0].y;
-
-      // with the catcher
-      if ((x >= x0-3 && x <= x0+3 && y>=y0-1 && y < y0+3)) {
-        score++;
-        kill_ball(i);
-      }
-      if ((x > x0-5 && x < x0-3 && y>=y0-2 && y < y0+1) ||
-        (x > x0+3 && x < x0+5 && y>=y0-2 && y < y0+1)) {
-        float by = sprites[i].ay;
-        float cy = sprites[0].ay;
-        sprites[i].ay += (cy-by);
-        sprites[0].ay += (by-cy);
-        sprites[i].y += sprites[i].ay;
-        sprites[0].y += sprites[0].ay;
-        float bx = sprites[i].ax;
-        float cx = sprites[0].ax;
-        sprites[i].ax += (cx-bx);
-        sprites[0].ax += (bx-cx);
-        sprites[i].x += sprites[i].ax;
-        sprites[0].x += sprites[0].ax;
-      }
-      // with the ground
-      if (y>=H-cH-2 && abs(sprites[i].ay) < .1) {
-        kill_ball(i);
-        balls_missed++;
-        score--;
-      }
-    }
-  }
-}
-
-float flap = 0;
-long dt;
-void catcher() {
-  if (level_up) {
-    reset_catcher();
-    if (balls_missed == 0) level++;
-    level_up = false;
-    display.clearDisplay();
-    display.setCursor(0,0);
-    display.print("-- Catcher! --");
-    if (balls_missed > 0) {
-      display.setCursor(20,10);
-      display.print("Missed ");
-      display.print(balls_missed);
-    }
-    balls_missed = 0;
-    display.setCursor(20,20);
-    display.print("Level ");
-    level_balls = level;
-    display.print(level);
-    display.display();
-    delay(2000);
-    ft = millis()+ ms_per_frame;
-
-    if (next_ball_max > 3000) next_ball_max -= 500;
-    bt = millis()+random(0,next_ball_max);
-    gravity += .001;
-    if (level > 1) {
-      wind = level * .05 + random(0,10)/20.0;
-      if (random(0,2)) wind *= -1;
-    }
-    else wind = 0;
-  }
-  dt = millis();
-  if (dt > ft) {
-    switch(pad_hit) {
-    case PAD_A+PAD_B:
-      catcher_init(); 
-      break;
-    case PAD_L:
-      if (sprites[CATCHER].ax < MAX_AX) sprites[CATCHER].ax+= R;
-      break;
-    case PAD_R:
-      if (sprites[CATCHER].ax > -MAX_AX) sprites[CATCHER].ax-= R;
-      break;
-    case PAD_U:
-      if (sprites[CATCHER].ay < MAX_AY) sprites[CATCHER].ay+= R;
-      break;
-    case PAD_D:
-      if (sprites[CATCHER].ay > -MAX_AY) sprites[CATCHER].ay-= R;
-      break;
-    }
-
-
-    ft+= ms_per_frame;
-    frame = (frame == FRAMES_PER_SECOND-1) ? 0 : frame+1;
-    display.clearDisplay();
-    display.setCursor(0,0);
-    display.print(score);
-    //display.print(" ");   display.print(wind);
-    //    display.print("ax:");display.print(sprites[0].ax);display.print(" ay:");display.print(sprites[0].ay);
-    //    display.print("x:");display.print(sprites[0].x);display.print(" y:");display.print(sprites[0].y);
-    for(int i=0;i< sprite_count;i++) {
-      display.drawBitmap((int)sprites[i].x, (int)sprites[i].y,  sprites[i].bitmap, sprites[i].w, sprites[i].h, 1);
-      if (sprites[i].draw_fun) (*sprites[i].draw_fun)(&sprites[i]);
-    }
-    if (wind != 0) {
-      display.drawBitmap(5,H-8,flags_bm[(int)flap],8,8,1);
-      if (frame&1) flap++;
-    }
-    else {
-      display.drawBitmap(5,H-8,flag0_bm,8,8,1);
-    }
-    if (flap > FLAG_COUNT) flap = 0;
-    display.drawLine(0,H-1,W-1,H-1,1);
-    display.display();
-    move();
-  }
-
-  // add balls in randomly according to lvel
-  if (dt > bt && level_balls > 0) {
-    level_balls--;
-    bt = millis()+random(0,next_ball_max);
-    add_ball();
-  }
-
-  if (pad_check()) {
-    if (pad_hit == PAD_A+PAD_B)
-      catcher_init(); 
-  }
-}
-
-void catcher_menu() {
-  display.setCursor(20,TITLE_Y);
-
-  display.print("Catcher!");
-}
 
 //---------------------------------------------------------------
 // DRAWER
@@ -960,102 +555,76 @@ void drawer_menu() {
   display.print("Drawer");
 }
 //---------------------------------------------------------------
-//UBER SNAKE
+//TEST
 
-const int max_length = 10;
-typedef struct{
-  uint8_t x;
-  uint8_t y;
-} Point;
-Point segs[max_length];
+const float pi = 3.14159265358979323846264338;
 
-int dir;
-int snake_speed;
-int seg_life;
-int growing;
+struct Cart {
+ int8_t x;
+ int8_t y;
+};
 
-int cur_seg;
-int d_seg;
-uint8_t applex;
-uint8_t appley;
+struct Polar {
+ int16_t angle;
+ float radius;
+};
 
-void newapple() {
-  applex = random(1,83);
-  appley = random(1,47);
-  display.drawPixel(applex,appley,1);
+Cart o = {42,24};
+struct Cart polar2cart(struct Polar p, int16_t r = 0) {
+  Cart c;
+  
+  float angler = deg2rad(p.angle+r);
+  
+  c.x = cos(angler) * p.radius;
+  c.y = sin(angler) * p.radius;
+
+  return c; 
 }
 
-void snake_init() {
-  growing = false;
-  snake_speed = 100;
-  seg_life = 3;
-  level = 1;
-  dir = PAD_R;
-  cur_seg = 2;
-  d_seg = 0;
-  
-  segs[0].x = 42;
-  segs[1].x = 43;
-  segs[2].x = 44;
+float deg2rad(uint16_t deg) {
+  return (2*pi)*deg/360;
+}
 
-  segs[0].y = 24;
-  segs[1].y = 24;
-  segs[2].y = 24;
+Polar sp [] = {{45, 10},{135, 10},{225, 10},{315, 10}};
 
+void drawsquare(uint16_t rot) {
+  uint8_t sqc = 0;
+  while (sqc <= 3) {
+    Cart c1 = polar2cart(sp[sqc], rot);
+    c1.x = c1.x + o.x;
+    c1.y = c1.y + o.y;
+    
+    Cart c2 = polar2cart(sp[(sqc<3)?sqc+1:0], rot);
+    c2.x = c2.x + o.x;
+    c2.y = c2.y + o.y;
+    
+    
+    display.drawLine(c1.x, c1.y, c2.x, c2.y, 1);
+    sqc++;
+  }
+}
+
+uint16_t rotv = 0;
+
+void test_init() {
+  Polar p = {45, 20};
+  Cart c = polar2cart(p);  
   display.clearDisplay();
   display.display();
-  
-  newapple();
-  
-  ft = millis()+snake_speed;
-  
-  randomSeed(ft);
-  
-  px = 44;
-  py = 24;
 
 }
 
-void snake() {
+void test() {
+  display.clearDisplay();
+  drawsquare(rotv);
+  rotv+=4;
+  display.display();
   
-  if((px == applex) && (py == appley)) { //apple is eaten
-    growing = true;
-    newapple();
-  }
-    
-  if(pad_check()) dir = pad_hit;
-  
-  //future time/speed
-  if(millis() >= ft) {
-    ft = millis()+snake_speed; //can be more efficient (refactor millis)
-    
-
-    if(dir == PAD_U)py--;
-    else if(dir == PAD_R)px++;
-    else if(dir == PAD_D)py++;
-    else if(dir == PAD_L)px--;
-    
-    display.drawPixel(px,py,1);
-    
-    cur_seg++;
-    if(cur_seg > max_length - 1) cur_seg = 0;
-    segs[cur_seg].x = px;
-    segs[cur_seg].y = py;
-    
-    display.drawPixel(segs[d_seg].x,segs[d_seg].y,0);
-    
-    if(!growing) d_seg++;
-    else growing = false;
-    
-    if(d_seg > max_length - 1) d_seg = 0;
-    
-    display.display();
-  }
 }
 
-void snake_menu() {
+void test_menu() {
   display.setCursor(13,TITLE_Y);
-  display.print("Uber Snake");
+  display.print("Testing-+*/ ^_^");
 }
 
 
